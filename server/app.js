4ce808ebc3a858b086366d7729600a09234eb430
Teigen Leonard <teigenl@gmail.com>
2017-03-26 20:08:22 -0500

    adds input and submit to HTML

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
+// requires and global variables
 var express = require ( 'express' );
 var app = express();
 var bodyParser = require( 'body-parser' );
 var port = 5000;
+
+//router
+var tasks = require( './routes/tasks.js' );
 
 
 //uses
 app.use( express.static( 'server/public/views' ));
 app.use( bodyParser.urlencoded( { extended: true }));
 
+app.use( '/tasks', tasks );
 
 //listen
 app.listen( port, function(){
